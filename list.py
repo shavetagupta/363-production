57c66c1e645d81ff16d3d12c4eb4e6252195a096
j648liu <55763556+j648liu@users.noreply.github.com>
2019-11-19 16:34:04 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+wl1 = ['forest','pocket','traffic','machine','leather',
+'lesson','branch','invention','station','education',
+'history','village','theatre','wagon','minute',
+'factory','direction','century','amount','record']
+
+wl2 = ['debate','furniture','wheel','address','judge',
+'ticket','account','powder','uniform','teacher',
+'answer','package','quarrel','victory','captain',
+'trousers','shoulder','afternoon','election','ocean']
+
+wlr = ['resort','laugh','market','capital','industry',
+'entrance','school','dinner','vacation','clothes',
+'partner','merchant','foundation','stream','garden',
+'kettle','winter','glass','beauty','queen']
+
+#event core visual
